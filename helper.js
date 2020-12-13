97fcfaf6cccece178958d83807c656fad9064b5b
Dimitri Glazkov <dimitri@glazkov.com>
2020-12-12 19:57:27 -0800

    Added the helper code.

---
+++ helper.js
@@ -0,0 +1,16 @@
+(() => {
+let rewind = document.querySelector('.adblFastRewind');
+if (!rewind) return;
+let forward = document.querySelector('.adblFastForward');
+if (!forward) return;
+
+if (!'mediaSession' in navigator) return;
+
+navigator.mediaSession.setActionHandler( 'previoustrack', () => {
+  rewind.click();
+});
+navigator.mediaSession.setActionHandler('nexttrack', () => {
+  forward.click();
+});
+
+})();
